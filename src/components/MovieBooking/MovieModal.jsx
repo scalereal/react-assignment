f102ce9afbf5fbf09ad9f36f8ab9c06135b9f366
aamir-islam <aamirislam38@gmail.com>
2022-07-14 21:56:33 +0530

    AI |constants inside separate folde

--- src/components/MovieBooking/MovieModal.jsx
+++ src/components/MovieBooking/MovieModal.jsx
@@ -1,69 +1,73 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import {
   ModelStyle,
   ImageStyles,
   ModeldataFlex,
   HeadingStyle,
-  Heading5Style,
+  SubHeadingStyle,
+  ModelScreenStyle,
   CloseButton,
   SeatHeading,
-  SeatHeadingH3,
+  SeatHeadingStyle,
 } from "../Styles/Modal.styles";
+import { imgUrl } from "../ constants/global";
 import CloseIcon from "../Assets/Xclose.png";
+const MovieModal = ({ propsData }) => {
+  const {  selectSeatArray, selectSeatFun, visibleFun } = propsData;
+  let param = useParams()
+  
 
-const MovieModal = ({ propsData, temp }) => {
-  const { poster_path, Fun, tempFun, setBackgroundFun } = propsData;
-
-  let total = temp.length * 250;
-  let SGST = (total * 12) / 100;
-
+  let total = selectSeatArray.length * 250;
+  let sgst = (total * 12) / 100;
   const CloseModal = () => {
-    Fun(false);
-    tempFun([]);
-    setBackgroundFun(true);
+    visibleFun(false);
+    selectSeatFun([]);
   };
   return (
+    <ModelScreenStyle>
     <ModelStyle>
       <CloseButton onClick={CloseModal}>
         <img src={CloseIcon} alt="close" />
       </CloseButton>
       <HeadingStyle>Thank you for booking</HeadingStyle>
-      <Heading5Style>Order Summary</Heading5Style>
-      <article style={{ margin: "48px 55px 48px 52px" }}>
+      <SubHeadingStyle>Order Summary</SubHeadingStyle>
+      <article >
         <ModeldataFlex>
           <article>
-            <ImageStyles src={poster_path} alt="movie" />
+            <ImageStyles src={ imgUrl+param.path } alt="movie" />
           </article>
           <article>
-            <SeatHeadingH3>Seats:</SeatHeadingH3>
+            <SeatHeadingStyle>Seats:</SeatHeadingStyle>
             <ModeldataFlex>
-              {temp.map((SeatNo, index) => (
+              {selectSeatArray.map((SeatNo, index) => (
                 <SeatHeading key={index}>{SeatNo},</SeatHeading>
               ))}
             </ModeldataFlex>
             <ModeldataFlex>
               <article>
-                <SeatHeading>{temp.length}*250:</SeatHeading>
+                <SeatHeading>{selectSeatArray.length}*250:</SeatHeading>
                 <span>SGST(12%):</span>
                 <br />
                 <span>CGST(12%):</span>
               </article>
               <article>
                 <SeatHeading>{total}</SeatHeading>
-                <span>{SGST}</span>
+                <span>{sgst}</span>
                 <br />
-                <span>{SGST}</span>
+                <span>{sgst}</span>
               </article>
             </ModeldataFlex>
             <hr />
             <ModeldataFlex>
               <SeatHeading>Total:</SeatHeading>
-              <SeatHeading>{total + SGST * 2}</SeatHeading>
+              <SeatHeading>{total + sgst * 2}</SeatHeading>
             </ModeldataFlex>
           </article>
         </ModeldataFlex>
       </article>
     </ModelStyle>
+    </ModelScreenStyle>
   );
 };
 
--- src/components/MovieList/Cards.jsx
+++ src/components/MovieList/Cards.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-
-import { useNavigate } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
 import {
   CardStyle,
   CardsStyle,
@@ -9,30 +7,21 @@ import {
   TitleStyle,
   CardsImageStyle,
 } from "../Styles/Card.style";
-
+import { imgUrl ,fakeImg } from "../ constants/global";
 const Cards = ({ data }) => {
-  const navigate = useNavigate();
-  const imgUrl = "https://image.tmdb.org/t/p/w500";
-  const fakeImg = "http://via.placeholder.com/1080x1580";
   return (
     <CardsStyle>
       {data.map((item) => {
         return (
           <CardStyle key={item.id}>
-              <CardsImageStyle
-                src={item.poster_path ?  imgUrl + item.poster_path : fakeImg }
-                alt={item.title}
-           />
+            <CardsImageStyle
+              src={item.poster_path ? imgUrl + item.poster_path : fakeImg}
+              alt={item.title}
+            />
             <TitleStyle>{item.title}</TitleStyle>
-            <ButtonStyle
-              onClick={(e) => {
-                navigate("/booking", {
-                  state: { path: imgUrl + item.poster_path, MovieId: item.id },
-                });
-              }}
-            >
-              Book Now
-            </ButtonStyle>
+            <NavLink to={`/booking/${item.id}${item.poster_path}`}>
+              <ButtonStyle>Book now</ButtonStyle>
+            </NavLink>
           </CardStyle>
         );
       })}
