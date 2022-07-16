5e8e5265e8f3986118a873296656234b7f638564
aamir-islam <aamirislam38@gmail.com>
2022-07-16 15:41:10 +0530

    AI | remove unnecessary logic

--- src/components/MovieBooking/BookingHome.jsx
+++ src/components/MovieBooking/BookingHome.jsx
@@ -1,29 +1,13 @@
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import React from "react";
 import MovieSeats from "./MovieSeats";
 import pic from "../Assets/Vector.png";
 import { SeatImgStyle, SeatMainDivStyle } from "../Styles/Seat.style";
 
 const BookingHome = () => {
-  let param = useParams()
-  const [background, setBackground] = useState(true);
-  
-  let white = {
-    backgroundColor: "white",
-  };
-  let grey = {
-    pointerEvents: "none",
-    backgroundColor: " rgba(229, 229, 229, 0.6)",
-  };
-
   return (
-    <SeatMainDivStyle style={background ? white : grey}>
+    <SeatMainDivStyle>
       <SeatImgStyle src={pic} alt="bg"></SeatImgStyle>
-      <MovieSeats
-        poster_Path={param.path}
-        MovieId={param.id}
-        setBackground={setBackground}
-      />
+      <MovieSeats />
     </SeatMainDivStyle>
   );
 };
