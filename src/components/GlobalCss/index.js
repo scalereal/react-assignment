a01596d3966e2e38b2fd38f5bb60f9fa0c4e6caf
Harish Karthik <harish.karthik@scalereal.com>
2022-07-29 23:13:50 +0530

    BTKT-1 | HK | style: add global css for fonts and reset

---
+++ src/components/GlobalCss/index.js
@@ -0,0 +1,1 @@
+export { GlobalCss as default } from "./component";
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import appTheme from "@theme";
+import { GlobalCss } from "@components";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <ThemeProvider theme={appTheme}>
+            <GlobalCss />
             <App />
         </ThemeProvider>
     </React.StrictMode>
