177e49322d738b5f091c460a332975846e243d25
Harish Karthik <harish.karthik@scalereal.com>
2022-07-29 22:36:35 +0530

    BTKT-1 | HK | style: add color theme for the application

---
+++ src/theme/colors/palette.js
@@ -0,0 +1,7 @@
+export const colors = {
+    brandPrimary: "#352C9A",
+    brandBlack: "#000000",
+    brandWhite: "#FFFFFF",
+    brandAccent: "#724FD8",
+    brandMute: "#626262",
+};
---
+++ src/theme/index.js
@@ -0,0 +1,5 @@
+import colors from "./colors";
+
+const appTheme = { colors };
+
+export default appTheme;
