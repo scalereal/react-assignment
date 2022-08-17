068f237b5381657077f4f4d871982fdec97a2956
Farah-Rashid <farahrashid54@gmail.com>
2022-08-17 10:36:09 +0530

    FR | moved constant file to src directory

---
+++ src/GlobalConstants.js
@@ -0,0 +1,11 @@
+export const imgUrl = "https://image.tmdb.org/t/p/w500/";
+export const fakeImg = "http://via.placeholder.com/1080x1580/";
+export const apiKey = "api_key=" + process.env.REACT_APP_API_KEY;
+export const baseUrl = "https://api.themoviedb.org/3";
+export const apiUrl =
+baseUrl + "/discover/movie?sort_by=popularity.desc&" + apiKey;
+export const searchUrl = baseUrl + "/search/movie?" + apiKey + "&query=";
+ export const data = {
+  id: ["A", "B", "C", "D"],
+  seats: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+};
