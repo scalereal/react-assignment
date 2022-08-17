ecf94965e7efbe97a4ab60eedc322b400c793674
Farah-Rashid <farahrashid54@gmail.com>
2022-08-17 11:36:30 +0530

    FR | added try catch block

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,51 +9,55 @@ import PageNotFound from "./PageNotFound";
 import { apiUrl, searchUrl } from "../GlobalConstants";
 
 const HomePage = () => {
-    const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const getMovies = async (url) => {
-        const response = await axios.get(url);
-        const result = response.data.results;
-        setMovies(result);
-    };
+  const getMovies = async (url) => {
+    try {
+      const response = await axios.get(url);
+      const result = response.data.results;
+      setMovies(result);
+    } catch (e) {
+      console.log("error", e);
+    }
+  };
 
-    const submit = (e) => {
-        e.preventDefault();
-        if (searchTerm) {
-            getMovies(`${searchUrl}&query=${searchTerm}`);
-        } else {
-            getMovies(apiUrl);
-        }
-    };
+  const submit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      getMovies(`${searchUrl}&query=${searchTerm}`);
+    } else {
+      getMovies(apiUrl);
+    }
+  };
 
-    useEffect(() => {
-        getMovies(apiUrl);
-    }, []);
-    return (
-        <>
-            <Container>
-                <h1>Book Tickets</h1>
-                <form onSubmit={submit}>
-                    <input
-                        type="text"
-                        placeholder="search"
-                        value={searchTerm}
-                        onChange={(e) => setSearchTerm(e.target.value)}
-                    />
-                </form>
-            </Container>
-            {movies.length > 0 ? (
-                <Cards>
-                    {movies.map((movie) => (
-                        <MovieCard movie={movie} key={movie.id} />
-                    ))}
-                </Cards>
-            ) : (
-                <PageNotFound />
-            )}
-        </>
-    );
+  useEffect(() => {
+    getMovies(apiUrl);
+  }, []);
+  return (
+    <>
+      <Container>
+        <h1>Book Tickets</h1>
+        <form onSubmit={submit}>
+          <input
+            type="text"
+            placeholder="search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </form>
+      </Container>
+      {movies.length > 0 ? (
+        <Cards>
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </Cards>
+      ) : (
+        <PageNotFound />
+      )}
+    </>
+  );
 };
 
 export default HomePage;
